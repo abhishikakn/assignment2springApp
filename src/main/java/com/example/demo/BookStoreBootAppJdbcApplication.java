e3919bc69bfdb4fc26825058a16f507c69770103
2606754 <abhishikakn@gmail.com>
2018-10-17 15:36:21 +0530

    second commit

---
+++ src/main/java/com/example/demo/BookStoreBootAppJdbcApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreBootAppJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreBootAppJdbcApplication.class, args);
+	}
+}
